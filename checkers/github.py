5ee4f1b73427e73b4306ccdf43d7b1f174ca54fc
caioluders <g3ol4d0@gmail.com>
2023-06-14 23:43:29 -0300

    fixing github username checker

--- checkers/github.py
+++ checkers/github.py
@@ -11,7 +11,12 @@ def check(url):
 	# get only user
 	if parsed_url.path.count('/') >= 2 :
 		url = '/'.join(url.split('/')[:4])
-	
+	username = url.split("/")[-1].lower()
+	#remove characters from username
+	allowed = "abcdefghijklmnopqrstuvwxyz0123456789-"
+	username = ''.join([ c for c in username if c in allowed ])
+	url = url.replace(url.split("/")[-1], username)
+
 	if url not in cache_domains :
 		cache_domains.add(url) 
 
